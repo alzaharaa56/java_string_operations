47e34872ce75babb82510e02c0b2ac66a64b7848
alzahara <shahdaljabri33@gmail.com>
2026-04-16 09:08:35 +0400

    update Task 15

--- src/StringReverseDemo.java
+++ src/StringReverseDemo.java
@@ -1,37 +1,18 @@
 public class StringReverseDemo {
     public static void main(String[] args) {
-
-        String original = "Java Programming";
-        System.out.println("Original String: " + original);
-
-
-        int length = original.length();
-        System.out.println("Total characters: " + length);
-
-
-        System.out.println("Characters one by one:");
-        for (int i = 0; i < length; i++) {
-            System.out.println(original.charAt(i));
-        }
-
-
-        String reversed = "";
-        for (int i = length - 1; i >= 0; i--) {
-            reversed += original.charAt(i);
-        }
-        System.out.println("Reversed String: " + reversed);
-
-
-        char target = 'a';
-        int count = 0;
-        for (int i = 0; i < length; i++) {
-            if (original.charAt(i) == target) {
-                count++;
+        String str ="java StringReverseDemo";
+        System.out.println(str);
+        System.out.println(str.length());
+        String reverseStr = "";
+        Integer ecounter = 0;
+        for(int i=str.length()-1; i > -1; i--){
+            System.out.println(str.charAt(i));
+            reverseStr += str.charAt(i);
+            if(str.charAt(i)=='e'){
+                ecounter++;
             }
         }
-        System.out.println("Character '" + target + "' appears: " + count + " times");
-
-
-        System.out.println("Comparison: " + original + "  " + reversed);
+        System.out.println("Reversed str: "+ reverseStr);
+        System.out.println("count of e in str: "+ ecounter);
     }
 }
